e0341aef71743000ca5e33182cf1fc82a93d0fd2
Varun Naik <varun@naikvarun.org>
2019-12-25 07:22:51 -0600

    Created the form for lat and long values

--- src/container/WeatherLookup.js
+++ src/container/WeatherLookup.js
@@ -1,12 +1,32 @@
 import React from "react";
+import {Col, Row, Form, Container} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 export default class WeatherLookup extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Weather lookup</h1>
-            </div>
+            <Container>
+                <Row>
+                    <Col className="justify-content-md-center">
+                        <Form>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridEmail">
+                                    <Form.Label>Latitude</Form.Label>
+                                    <Form.Control type="number" placeholder="0" min={-90} max={90}/>
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridPassword">
+                                    <Form.Label>Longitude</Form.Label>
+                                    <Form.Control type="number" placeholder="90" min={-180} max={180}/>
+                                </Form.Group>
+                            </Form.Row>
+                            <Button variant="primary" type="submit">Submit</Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+
         );
     }
 }
