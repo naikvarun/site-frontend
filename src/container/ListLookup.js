2d10eb0a7044e90c5d10c2010ca9d63a29bd364d
Varun Naik <varun@naikvarun.org>
2019-12-25 21:16:50 -0600

    added font and cleanup display for accordian

--- src/container/ListLookup.js
+++ src/container/ListLookup.js
@@ -2,7 +2,8 @@ import React from "react";
 import WeatherService from "../service/weather-api";
 import DisplayWeather from "./DisplayWeather";
 import {Accordion, Button, Card} from "react-bootstrap";
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faCaretSquareDown } from '@fortawesome/free-solid-svg-icons'
 export default class ListLookup extends React.Component {
 
     constructor(props) {
@@ -20,19 +21,18 @@ export default class ListLookup extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="mt-3">
                 <Accordion>
                     {this.state.previousLookups.map((info, index) => {
                         return (
                             <Card>
                                 <Card.Header>
-                                    <Accordion.Toggle as={Button} variant="link" eventKey={index}>
-                                        <div>
-                                            <span>Latitude: {info.latitude}</span>
-                                            <span className="ml-2">Longitude: {info.longitude}</span>
-                                        </div>
+                                    <Accordion.Toggle eventKey={index}>
+                                        <FontAwesomeIcon icon={faCaretSquareDown} />
                                     </Accordion.Toggle>
-
+                                    <span className="ml-2">Latitude: {info.latitude}</span>
+                                    <span className="ml-2 mr-2">Longitude: {info.longitude}</span>
+                                    <div className="float-right">Retrieved at {new Date(info.requestedTime).toLocaleString()}</div>
                                 </Card.Header>
                                 <Accordion.Collapse eventKey={index}>
                                     <Card.Body>
